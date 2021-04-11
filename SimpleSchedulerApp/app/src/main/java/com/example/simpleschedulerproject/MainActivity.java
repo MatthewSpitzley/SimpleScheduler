e6845a94563a763674d9e7eb645faa250ad3d762
MatthewSpitzley <74800314+MatthewSpitzley@users.noreply.github.com>
2021-04-11 17:08:28 -0500

    added a gear icon for the settings button edited the manifest to make sure the main activity opens first edited main activity to use the settings button created settings and edited related files

--- SimpleSchedulerApp/app/src/main/java/com/example/simpleschedulerproject/MainActivity.java
+++ SimpleSchedulerApp/app/src/main/java/com/example/simpleschedulerproject/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.simpleschedulerproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsBtn = findViewById(R.id.settingsButton);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- SimpleSchedulerApp/app/src/main/java/com/example/simpleschedulerproject/Settings.java
+++ SimpleSchedulerApp/app/src/main/java/com/example/simpleschedulerproject/Settings.java
@@ -19,59 +19,29 @@ public class Settings {
                 "\t To create a new category, continue from the Category List and select the option 'New Category' \n" +
                 "To access the Settings screen select the button in the top right");
     }
-    /*public String format12Hr(){
-        return "hh:mm aa";
-    }
 
-    public String format24Hr(){
-        return "HH:mm";
-    }
-
-    public String monthDayYear(){
-        return "MM/dd/yyyy";
-    }
-
-    public String dayMonthYear(){
-        return "dd/MM/yyyy";
-    }
-
-    public String abbMonthDayYear(){
-        return "MMM dd yyyy";
-    }
-
-    public String dayAbbMonthYear(){
-        return "dd MMM yyyy";
-    }
-
-    public String fullMonthDayYear(){
-        return "MMMM dd yyyy";
-    }
-
-    public String dayFullMonthYear(){
-        return "dd MMMM yyyy";
-    }*/
     //changes between 12 and 24 hour times
-    public DateTimeFormatter toggleTime(int x){
+    public DateTimeFormatter toggleTime(boolean x){
         DateTimeFormatter df = DateTimeFormatter.ofPattern("hh:mm aa");
-        switch(x){
-            case 0: //12 hour format
-                df = DateTimeFormatter.ofPattern("hh:mm aa");
-                break;
-            case 1: //24 hour format
-                df = DateTimeFormatter.ofPattern("HH:mm");
-                break;
+        if(!x){
+            //12 hr
+            df = DateTimeFormatter.ofPattern("hh:mm aa");
+        }
+        else{
+            //24 hr
+            df = DateTimeFormatter.ofPattern("HH:mm");
         }
         return df;
     }
 
     //changes the main screen to either the task list or calendar screen
     public void switchScreen(){
-
+        //if true (default) do nothing, if false as the main activity launches make it redirect to the calendar screen on startup
     }
 
    //allows the user to change the time before a task occurs to send a reminder notification
-    public void notificationTiming(){
-
+    public int notificationTiming(int x){
+        return 60;
     }
 
     //user changes how they prefer to see dates displayed
@@ -112,7 +82,11 @@ public class Settings {
     }
 
     //user can choose whether or not to receive push and/or email notifications
-    public void notificationType(){
+    public void notificationEmail(){
+
+    }
+
+    public void notificationPush(){
 
     }
 
